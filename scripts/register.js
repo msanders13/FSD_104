aa2dbea58d9997d3ffc83b9d826b909b8119eebe
msanders13 <97141929+msanders13@users.noreply.github.com>
2022-02-11 14:35:25 -0600

    Changes after class 4
    Added the table and styled
    class 4 changes

--- scripts/register.js
+++ scripts/register.js
@@ -11,58 +11,125 @@ let salon={
         close:"5:00 pm"
     },
 
-    pets:[
-        {
-            name:"Scooby",
-            age:60,
-            gender:"male",
-            breed:"Dane",
-            service:"Grooming",
-            owner:"Shaggy",
-            phone:"555-555-5555"
-        },
-        {
-            name:"Lassie",
-            age:20,
-            gender:"female",
-            breed:"Collie",
-            service:"Nail Trim",
-            owner:"Timmy",
-            phone:"555-555-5554"
-        },
-        {
-            name:"Snoopy",
-            age:90,
-            gender:"male",
-            breed:"Beagle",
-            service:"Shots",
-            owner:"Charlie",
-            phone:"555-555-5553"
-        },
-        {
-            name:"McGruff",
-            age:100,
-            gender:"male",
-            breed:"Bulldog",
-            service:"Wash",
-            owner:"Officer Friendly",
-            phone:"555-555-5552"
-        }
-    
-    ]
+    pets:[]
 }
 
-let petNames = ["Scooby","Lassie","Snoopy","McGrufff"]; //need to have this pull from petsRegistered
 
 function displaypetNames(){
+    alert(`You have 4{salon.pets.length} pets.`);
     for(let i=0;i<1;i++){
-        document.write(petNames);
-        
+        console.log(salon.pets[i]);
+    }    
+}    
+
+//create the constructor
+function Pet(name,age,gender,breed,service,petOwnerContact){
+    this.name=name;
+    this.age=age;
+    this.gender=gender;
+    this.breed=breed;
+    this.service=service;
+    this.petOwnerContact=petOwnerContact;
+}
+
+
+
+let petName = document.getElementById("txtName");
+let petAge = document.getElementById("txtAge");
+let petGender = document.getElementById("txtGender");
+let petBreed = document.getElementById("txtBreed");
+let petService = document.getElementById("txtService");
+let petOwnerContact = document.getElementById("txtpetOwnerContact");
+
+function register(){
+
+
+
+    console.log("Register");
+    //create an obj
+    let thePet = new Pet(petName.value,petAge.value,petGender.value,petBreed.value,petService.value,petOwnerContact.value)
+    console.log();
+    //push the obj into the array
+    salon.pets.push(thePet);
+    //clear inputs
+    clearInputs();
+    //display the array
+    console.log(salon.pets);
+    //alert
+    showPetsCards();
+    alert("You have registered a new pet!");
+}
+
+function clearInputs(){
+    document.getElementById("txtName").value="";
+    document.getElementById("txtAge").value="";
+    document.getElementById("txtGender").value="";
+    document.getElementById("txtBreed").value="";
+    document.getElementById("txtService").value="";
+    document.getElementById("txtpetOwnerContact").value="";
+}
+
+function showPetsCards (){
+    document.getElementById('btnClear').hidden = true;
+    document.getElementById('headerList').innerHTML="Pets List";
+    //clear the field
+    document.getElementById("petList").innerHTML="";
+    //travel the array for loop
+    for(let i=0; i<salon.pets.length; i++){
+    //create the card
+    console.log(createCard(salon.pets[i],i));
+    //append the card into the HTML
+    document.getElementById("petList").innerHTML += createCard(salon.pets[i], i);
+
+    }
+    
+}
+
+function removePet(index){
+    if(index <salon.pets.length)
+    {
+    salon.pets.splice(index, 1);
+    }
+    showPetsCards();
+}
+
+function searchPet(){
+    document.getElementById('btnClear').hidden = false;
+    let name = document.getElementById('txtSearch').value;
+    document.getElementById('headerList').innerHTML = "Search Result:";
+    document.getElementById("petList").innerHTML="";
+    //travel the array for loop
+    for(let i=0; i<salon.pets.length; i++){
+    //create the card
+    console.log(createCard(salon.pets[i],i));
+        //append the card into the HTML
+        if(name.toLowerCase() == salon.pets[i].name.toLowerCase())
+        {
+            document.getElementById("petList").innerHTML += createCard(salon.pets[i], i);
+        }
     }
 }
-displaypetNames();
 
+function createCard(pet, index=0){
+    return `
+    <div class="pet-card">
+        <h3>${pet.name}</h3>
+        <label>Age: ${pet.age}</label>
+        <label>Gender: ${pet.gender}</label>
+        <label>Breed: ${pet.breed}</label>
+        <label>Service: ${pet.service}</label>
+        <label>Owner Contact: ${pet.petOwnerContact}</label>
+        <button onclick="removePet(${index});">Remove</button>
+    </div>
+    `;
+}
 
-
-window.confirm("There are 4 pets registered"); //might need to add variable for number of pets in salon.pet
-
+function init(){
+    //create 3 pets using the literal constructor
+    let scrappy = new Pet("Scrappy",50,"Male","Dane","Grooming","ContactInfo");
+    let snoopy = new Pet("Snoopy",40,"Male","Beagle","Nails","Contactinfo");
+    let zeus = new Pet("Zeus",7,"Male","Dachsund","Wash","ContactInfo");
+    salon.pets.push(scrappy,snoopy,zeus); //push elements into the array
+    showPetsCards();
+}
+window.onload=init;
